47f660619ec442f57ecaddab2841bf6a5833ec2f
peugeotdude505 <44513072+peugeotdude505@users.noreply.github.com>
2020-02-14 10:35:50 -0800

    Add files via upload

--- sort_ips_3cols.py
+++ sort_ips_3cols.py
@@ -3,6 +3,7 @@
 #See also https://docs.python.org/3/library/socket.html#socket.inet_aton
 
 import socket
+
 output = open('sorted_output.txt',"w+")
 results = []
 counter = 0
@@ -11,15 +12,17 @@ with open('hosts_to_sort.txt') as f:
     for line in f:
         info = line.split()
         counter = counter+1
-        try:
-            results.append((info[0],info[1]))
-        except IndexError:
-            print('Bad input at line ', counter)
+            try:
+                results.append((info[0],info[1],info[2]))
+            except IndexError:
+                print('Bad input at line ', counter)
 
 
 #See also https://docs.python.org/3/library/socket.html#socket.inet_aton
-for (ip, hostname) in sorted(results, key=lambda item: socket.inet_aton(item[0])):
-    print ((ip.ljust(20,' ')), ' ' ,(hostname.ljust(30,' ')))
-    output.write ((ip.ljust(20,' ')) + ' # ' + (hostname.ljust(30,' ')) + '\n')
-     
+for (ip, hostname,dns) in sorted(results, key=lambda item: socket.inet_aton(item[0])):
+    
+    print ((ip.ljust(20,' ')), ' ' ,(hostname.ljust(30,' ')), ' ' ,dns)
+    output.write ((ip.ljust(20,' ')) + ' # ' + (hostname.ljust(30,' ')) + ' ' + dns + '\n')
+  
+  
 output.close()
